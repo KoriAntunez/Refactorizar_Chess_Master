a4dbd08d38661eaaba39535266b81d531c593ff3
rafael18599 <55866974+rafael18599@users.noreply.github.com>
2022-08-04 17:42:42 -0500

    refactorizar Bishop.java

--- Original.src/Bishop.java
+++ Original.src/Bishop.java
@@ -36,17 +36,30 @@ public class Bishop extends ChessGamePiece{
     @Override
     protected ArrayList<String> calculatePossibleMoves( ChessGameBoard board ){
         //se puede refactorizar
+        /*ArrayList<String> northEastMoves = calculateNorthEastMoves( board, 8 );
+        ArrayList<String> northWestMoves = calculateNorthWestMoves( board, 8 );
+        ArrayList<String> southEastMoves = calculateSouthEastMoves( board, 8 );
+        ArrayList<String> southWestMoves = calculateSouthWestMoves( board, 8 );*/
+
+        //Se emplea un Método para calcular todos los posibles movimientos
+        ArrayList<String> allMoves = MovimientosCalculados(board);
+        //ArrayList<String> allMoves = new ArrayList<String>();
+        //allMoves.addAll( calcularMoves );
+        return allMoves;
+    }
+
+    public ArrayList<String> MovimientosCalculados (ChessGameBoard board){
         ArrayList<String> northEastMoves = calculateNorthEastMoves( board, 8 );
         ArrayList<String> northWestMoves = calculateNorthWestMoves( board, 8 );
         ArrayList<String> southEastMoves = calculateSouthEastMoves( board, 8 );
         ArrayList<String> southWestMoves = calculateSouthWestMoves( board, 8 );
-        ArrayList<String> allMoves = new ArrayList<String>();
-        allMoves.addAll( northEastMoves );
-        allMoves.addAll( northWestMoves );
-        allMoves.addAll( southEastMoves );
-        allMoves.addAll( southWestMoves );
-        return allMoves;
-    }
+        ArrayList<String> resultMocves = new ArrayList<String>();
+        resultMocves.addAll(northEastMoves);
+        resultMocves.addAll(northWestMoves);
+        resultMocves.addAll(southEastMoves);
+        resultMocves.addAll(southWestMoves);
+        return resultMocves;
+    } 
     /**
      * Creates an icon for this piece depending on the piece's color.
      * 
